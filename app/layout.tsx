4e06b0ba958e5478c5488c5697cbb79b658503fc
Sumit Suthar <71690208+sumit171204@users.noreply.github.com>
2024-11-16 00:13:26 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ import './globals.css';
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://chat.vercel.ai'),
-  title: 'Next.js Chatbot Template',
-  description: 'Next.js chatbot template using the AI SDK.',
+  title: 'SmartGPT',
+  description: 'SmarGPT using the AI SDK.',
 };
 
 export const viewport = {
